602f54890c6b8084afe5799d2b60aa0bd9d71ac5
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-04-29 16:35:06 +0530

    Used all the magic

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,24 +1,83 @@
 import express from 'express';
+import cors from 'cors';
+import log from './core/logger'
+import logger from './middleware/logger.middleware';
+import corsOptions from './config/cors.config';
+import Route from './core/route';
+import EnvVar from './core/envvar';
+import * as controllers from './controllers';
 
-import routes from './routes/routes';
+import mongoose from 'mongoose';
 
 class App {
-  public server;
+  private server;
+  private port = 3500;
 
-  constructor() {
+  public constructor() {
+
+    // Connect to the MongoDB database
+    mongoose.connect(EnvVar.Instance.get('BACKEND_DATABASE_URI'))
+      .then(() => {
+        log.info('(MONGOOSE): Connected to the MongoDB database', 'database.log');
+      })
+      .catch((err) => {
+        log.error(`(MONGOOSE): Error connecting to the MongoDB database - ${err}`, 'database.log');
+      });
+
+    mongoose.connection.on('error', (err) => {
+      log.error(`(MONGOOSE): ${err}`, 'database.log');
+    });
+
+    mongoose.connection.on('all', (info) => {
+      log.info(`(MONGOOSE): ${info}`, 'database.log');
+    });
+
+    if (EnvVar.Instance.get('BACKEND_DEBUG') === 'true') {
+      mongoose.set("debug", (collectionName, method, query, doc) => {
+        log.info(`(MONGOOSE): ${collectionName}.${method} ${JSON.stringify(query)}, ${doc}`, 'database.log');
+      });
+    }
+
+    // Set the port from the environment variables or use the default port
+    this.port = parseInt(EnvVar.Instance.get('BACKEND_EXPRESS_PORT')) || this.port;
+
     this.server = express();
 
     this.middlewares();
     this.routes();
+
+    // Add the errorLogger middleware here in order to log all the errors that occur in the application
+    this.server.use(logger.errorLogger);
   }
 
-  middlewares() {
+  private middlewares() {
+    this.server.use(logger.requestLogger);
+    this.server.use(express.urlencoded({ extended: true }));
+
+    // Only allow json requests
     this.server.use(express.json());
+
+    // Enable CORS
+    this.server.use(cors(corsOptions));
   }
 
-  routes() {
-    this.server.use(routes);
+  private routes() {
+    const routes = Object.values(controllers).map((controller) => new Route(controller));
+    routes.forEach((route) => {
+      this.server.use(route.getRouter());
+    });
+    this.server.all('*', (req, res) => {
+      res.status(404).json({ message: 'Route not found' });
+    });
+  }
+
+  public listen() {
+    mongoose.connection.once('open', () => {
+      this.server.listen(this.port, () => {
+        log.info(`Server is running on port ${this.port}`, 'server.log');
+      });
+    });
   }
 }
 
-export default new App().server;
+export default new App();
